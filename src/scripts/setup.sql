2b3c2e807a98ee204b82c96e59c03c0ec0066328
Umair <Umair@10.50.138.169>
2014-09-19 14:44:39 +0200

    LOL

---
+++ src/scripts/setup.sql
@@ -0,0 +1,12 @@
+DROP TABLE Person;
+CREATE TABLE PERSON (
+ ID INTEGER NOT NULL,
+ FIRST_NAME VARCHAR(50),
+ LAST_NAME VARCHAR(50),
+ PRIMARY KEY (ID)
+);
+insert into Person values(100,'Peter','Hansen');
+insert into Person values(110,'Lone','Hansen');
+insert into Person values(120,'John','McDonald');
+insert into Person values(130,'Peter','Jensen');
+

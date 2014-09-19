245cee7dc99f8711499b5d2eddb5ab42b9523b5e
Umair <Umair@10.50.138.169>
2014-09-19 14:34:04 +0200

    LOL

---
+++ src/databasedemo/DatabaseDemo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databasedemo;
+
+/**
+ *
+ * @author Umair
+ */
+public class DatabaseDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
